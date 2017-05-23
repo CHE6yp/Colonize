32ff7a0cbc1ba90ba4e02b516f60f0750e734357
ARRRR <che6yp@gmail.com>
2017-05-23 18:48:39 +0400

    Created Robber script

---
+++ Assets/Scripts/Map/Robber.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Robber : MonoBehaviour {
+
+    public static Robber singleton;
+
+
+	// Use this for initialization
+	void Awake () {
+        singleton = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
